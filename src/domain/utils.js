89ab3789cea444894dc5be0b8dc918777fe8beb1
Ruan <gato@icomp.ufam.edu.br>
2020-07-27 16:29:52 -0400

    [UPD] Recovering array objects

--- src/domain/utils.js
+++ src/domain/utils.js
@@ -1,96 +1,96 @@
 function normalizeHTMLString(str) {
-  return str.replace(/[\n|\t]/g, '').replace(/ +/g, ' ').trim();
+    return str.replace(/[\n|\t]/g, '').replace(/ +/g, ' ').trim();
 }
 
 function getInnerText(page, selector, context) {
-  return normalizeHTMLString(page(selector, context).text());
+    return normalizeHTMLString(page(selector, context).text());
 }
 
 function parseEntities(text, delimiter = ':') {
-  const shattered = [].concat.apply([], (text.split(delimiter).map((str) => str.split('<>').map(normalizeHTMLString).filter((e) => e.length > 0))));
-  const entities = {};
-  for (let i = 0; i < shattered.length; i += 2) {
-    const entityName = shattered[i];
-    const entityValue = shattered[i + 1];
-    if (!(entityName in entities)) {
-      entities[entityName] = [];
+    const shattered = [].concat.apply([], (text.split(delimiter).map((str) => str.split('<>').map(normalizeHTMLString).filter((e) => e.length > 0))));
+    const entities = {};
+    for (let i = 0; i < shattered.length; i += 2) {
+        const entityName = shattered[i];
+        const entityValue = shattered[i + 1];
+        if (!(entityName in entities)) {
+            entities[entityName] = [];
+        }
+        entities[entityName].push(entityValue);
     }
-    entities[entityName].push(entityValue);
-  }
 
-  return entities;
+    return entities;
 }
 
-function getEntitiesFromString(str, delimiter = ':', { raw } = { raw: false }) {
-  const text = str.replace(/\n+/g, '<>').replace(/ +/, ' ').replace(/(\d+):(\d+)/g, '$1<comma>$2')
-    .trim();
+function getEntitiesFromString(str, delimiter = ':', {raw} = {raw: false}) {
+    const text = str.replace(/\n+/g, '<>').replace(/ +/, ' ').replace(/(\d+):(\d+)/g, '$1<comma>$2')
+        .trim();
 
-  const entities = parseEntities(text, delimiter);
+    const entities = parseEntities(text, delimiter);
 
-  if (raw) {
-    return entities;
-  }
-
-  function gather(keyList, merge = false) {
-    if (Array.isArray(keyList)) {
-      if (merge) {
-        let merged = [];
-        for (const key of keyList) {
-          if (key in entities) {
-            merged = merged.concat(entities[key]);
-          }
-        }
-        return merged;
-      }
-      for (const key of keyList) {
-        if (key in entities) {
-          return entities[key];
-        }
-      }
+    if (raw) {
+        return entities;
     }
-    return entities[keyList];
-  }
 
-  return gather;
+    function gather(keyList, merge = false) {
+        if (Array.isArray(keyList)) {
+            if (merge) {
+                let merged = [];
+                for (const key of keyList) {
+                    if (key in entities) {
+                        merged = merged.concat(entities[key]);
+                    }
+                }
+                return merged.length > 0 ? merged : undefined;
+            }
+            for (const key of keyList) {
+                if (key in entities) {
+                    return entities[key];
+                }
+            }
+        }
+        return entities[keyList];
+    }
+
+    return gather;
 }
 
-function getEntities(page, selector, delimiter = ':', { context, raw } = { context: undefined, raw: false }) {
-  const text = page(selector, context).text().replace(/\n+/g, '<>').replace(/ +/, ' ')
-    .replace(/(\d+):(\d+)/g, '$1<comma>$2')
-    .trim();
+function getEntities(page, selector, delimiter = ':', {context, raw} = {context: undefined, raw: false}) {
+    const text = page(selector, context).text().replace(/\n+/g, '<>').replace(/ +/, ' ')
+        .replace(/(\d+):(\d+)/g, '$1<comma>$2')
+        .trim();
 
-  const entities = parseEntities(text, delimiter);
+    const entities = parseEntities(text, delimiter);
 
-  if (raw) {
-    return entities;
-  }
-
-  function gather(keyList, merge = false) {
-    if (Array.isArray(keyList)) {
-      if (merge) {
-        let merged = [];
-        for (const key of keyList) {
-          if (key in entities) {
-            merged = merged.concat(entities[key]);
-          }
-        }
-        return merged;
-      }
-      for (const key of keyList) {
-        if (key in entities) {
-          return entities[key];
-        }
-      }
+    if (raw) {
+        return entities;
     }
-    return entities[keyList];
-  }
 
-  return gather;
+    function gather(keyList, merge = false) {
+        if (Array.isArray(keyList)) {
+            if (merge) {
+                let merged = [];
+                for (const key of keyList) {
+                    if (key in entities) {
+                        merged = merged.concat(entities[key]);
+                    }
+                }
+                return merged.length > 0 ? merged : undefined;
+            }
+            for (const key of keyList) {
+                if (key in entities) {
+                    return entities[key];
+                }
+            }
+        }
+        return entities[keyList];
+    }
+
+    return gather;
 }
 
 module.exports = {
-  getEntitiesFromString,
-  normalizeHTMLString,
-  getInnerText,
-  getEntities,
+    getEntitiesFromString,
+    normalizeHTMLString,
+    getInnerText,
+    getEntities,
 };
