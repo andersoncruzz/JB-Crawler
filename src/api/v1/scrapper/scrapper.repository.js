2794772d102f186fbf20b12b58b82ba3b96894f8
Ruan <gato@icomp.ufam.edu.br>
2020-08-04 15:58:33 -0400

    [DLT] Useless test code

--- src/api/v1/scrapper/scrapper.repository.js
+++ src/api/v1/scrapper/scrapper.repository.js
@@ -8,11 +8,6 @@ const log = require('../../../logger');
 
 async function resolvePage(instance, processId, save = false) {
     const instancePage = await client.navigate(instance.url(processId));
-
-    if (process.argv.indexOf('--save-to-test') >= 0 && save) {
-        const url = encodeURIComponent(instance.url(processId));
-        saveDebugger(url, 'response', instancePage, 'extra-mocks');
-    }
     const parsedEntry = instance.parser(cheerio.load(instancePage));
 
     if (parsedEntry && Array.isArray(parsedEntry.subprocessos)) {
@@ -26,19 +21,6 @@ async function resolvePage(instance, processId, save = false) {
     return parsedEntry;
 }
 
-function saveDebugger(sanitizedProcessId, filename, content, folder) {
-    const path = require('path');
-    const fs = require('fs');
-    const primaryFolder = path.resolve(__dirname, `../../../../test/mocks/${folder}`);
-    const secondaryFolder = path.resolve(__dirname, `${primaryFolder}/${sanitizedProcessId}`);
-
-    if (!fs.existsSync(secondaryFolder)) {
-        fs.mkdirSync(secondaryFolder);
-    }
-
-    fs.writeFileSync(`${secondaryFolder}/${filename}`, content);
-}
-
 async function find(processId, follow) {
     const state = findStateFromTRCode(processId);
     const sanitizedProcessId = Sanitizer.sanitize(processId);
@@ -50,12 +32,6 @@ async function find(processId, follow) {
         primeiraInstancia: firstInstanceParsedPage,
         segundaInstancia: secondInstanceParsedPage,
     };
-
-    if (process.argv.indexOf('--save-to-test') >= 0) {
-        saveDebugger(sanitizedProcessId, 'firstInstance.html', await client.navigate(state.instances.first.url(sanitizedProcessId)), 'requests');
-        saveDebugger(sanitizedProcessId, 'secondInstance.html', await client.navigate(state.instances.second.url(sanitizedProcessId)), 'requests');
-        saveDebugger(sanitizedProcessId, 'response.json', JSON.stringify(response), 'requests');
-    }
 
     if (!(response.primeiraInstancia || response.segundaInstancia)) {
         throw new InquiredDocumentNotFoundException('Não foi possível encontrar dados para esse documento.');
